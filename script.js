4fb322754d4b1f9230f69ee253ef7b2f5d2010e2
gkilasonia <dgiorjio@gmail.com>
2025-01-21 14:25:51 +0400

    Descktop version done

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const shareSection = document.getElementById("share-section");
 
 btnOpen.addEventListener("click", function () {
   shareSection.style.display = "flex";
+  btnOpen.style.display = "none";
 });
 
 btnClose.addEventListener("click", function () {
   shareSection.style.display = "none";
+  btnOpen.style.display = "initial";
 });
